e574654af939ff6cfd4135d89d19b2dbd1ea739f
Marc J Swanson <marcjswanson@hotmail.com>
2020-09-06 07:30:41 -0500

    copy code implemented

--- 003_DataCopyFlash/Src/main.c
+++ 003_DataCopyFlash/Src/main.c
@@ -19,6 +19,7 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
@@ -31,9 +32,28 @@ void splash()
 	printf("**************************\n");
 }
 
+// this is a string literal (constant data) and will be stored in ROM (Flash memory)
+const char *gpMessage = "Hi there, and good morning!";
+
+// this is constant data.  This data will be stored in ROM (Flash memory)
+const int gValue = 100;
+
+// this is not constant data and will be stoed in RAM
+char gMyData[50];
+
+
+
 int main(void)
 {
 	splash();
+
+	for ( uint32_t i = 0; i < strlen( gpMessage); i++ )
+	{
+		// this data is copied from ROM (Flash) to RAM (SRAM)
+		gMyData[i] = *(gpMessage + i);
+	}
+
+
 
     /* Loop forever */
 	for(;;);
